3a54491d2b000629453269cdd765e690d96b0234
Mac Heller-Ogden <mac@machellerogden.com>
2019-11-12 12:53:37 -0600

    refactor how we source config and state machine data

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const sota = require('sota');
 const { Trajectory } = require('trajectory');
 const DefaultAnswers = require('answers');
 const callsites = require('callsites');
-const constants = require('./lib/constants');
-const { debug, print_machine } = require('./lib/debug');
+const { debug, print } = require('./lib/debug');
 const { load, sourceExpander } = require('./lib/load');
 const { optionsSchema, configSchema } = require('./lib/schema');
-const { prefixOptions } = require('./lib/util');
+const { prefixOptions, setEnv } = require('./lib/util');
 const { resolveTarget, loadResource, SubmachineResolver } = require('./lib/resolve');
 const { handleError } = require('./lib/error');
 const { EOL } = require('os');
@@ -27,6 +26,17 @@ async function Incant(options = {}) {
             __Answers__:Answers = DefaultAnswers
         } = await optionsSchema.validate(options);
 
+        const cliOptionsIdx = argv.indexOf('--');
+        const hasCliOptions = cliOptionsIdx == -1;
+
+        const cliStates = hasCliOptions
+            ? argv
+            : argv.slice(0, cliOptionsIdx);
+
+        const cliOptions = hasCliOptions
+            ? []
+            : argv.slice(cliOptionsIdx + 1);
+
         process.title = name;
 
         /**
@@ -34,20 +44,20 @@ async function Incant(options = {}) {
          */
         const config = await configSchema.validate(await Answers({
             name,
-            argv: prefixOptions(argv),
+            argv: cliOptions,
             loaders: [ sourceExpander ]
         }));
 
-        const DEBUG = process.env[constants.DEBUG] || config['--'].includes('--debug') || (config.settings || {}).debug;
-        const PRINT_MACHINE = process.env[constants.PRINT_MACHINE] || config['--'].includes('--print-machine') || (config.settings || {}).print_machine;
-        const VERBOSE = process.env[constants.VERBOSE] || config['--'].includes('-v') || config['--'].includes('--verbose') || (config.settings || {}).verbose;
-        const COMPACT = process.env[constants.COMPACT] || config['--'].includes('-c') || config['--'].includes('--compact') || (config.settings || {}).compact;
-        const PIPE = process.env[constants.PIPE] || config['--'].includes('-p') || config['--'].includes('--pipe') || (config.settings || {}).pipe;
-        if (DEBUG) process.env[constants.DEBUG] = DEBUG;
-        if (PRINT_MACHINE) process.env[constants.PRINT_MACHINE] = PRINT_MACHINE;
+        const {
+            DEBUG,
+            PRINT,
+            VERBOSE,
+            QUIET,
+            PIPE
+        } = setEnv(config);
 
         debug('CONFIG', config);
-        if (config._.length === 0) {
+        if (cliStates.length === 0) {
             process.stdout.write(`nothing to do...${EOL}`);
             return;
         }
@@ -58,11 +68,11 @@ async function Incant(options = {}) {
         const source = [ ...givenSource, ...config.source.flat(Infinity) ];
         const loadedTargets = await load({ patterns: source, cwd: path.dirname(calledFrom) });
         const targets = { ...givenTargets, ...loadedTargets }
-        const machine = await sota.readAll(config._, { resolver: SubmachineResolver(targets), argv: true });
+        const machine = await sota.readAll(cliStates, { resolver: SubmachineResolver(targets), argv: true });
 
         debug('STATE MACHINE DEFINITION', machine);
-        print_machine(machine);
-        if (PRINT_MACHINE) process.exit(0);
+        print(machine);
+        if (PRINT) process.exit(0);
 
         /**
          * Trajectory - execute state machine
@@ -80,7 +90,7 @@ async function Incant(options = {}) {
         const trajectoryOptions = {
             reporterOptions: {
                 cols: 0,
-                compact: PIPE || COMPACT,
+                quiet: PIPE || QUIET,
                 gutterWidth: 12,
                 printEvents: {
                     succeed: !PIPE,
